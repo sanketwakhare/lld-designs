40aa9a1ebbb454c877d8cabd94db076787fb0a1f
sanketwakhare <sanketwakhare@gmail.com>
2022-07-20 15:55:01 +0530

    add config files for module

---
+++ design-pen/src/main/java/com/sanket/designpen/DesignPenApplication.java
@@ -0,0 +1,13 @@
+package com.sanket.designpen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPenApplication.class, args);
+    }
+
+}
