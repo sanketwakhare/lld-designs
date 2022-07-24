97519bed1254b67d4f8f5efd2df44d0ebd1fd83b
sanketwakhare <sanketwakhare@gmail.com>
2022-07-24 18:19:14 +0530

    fix module settings

---
+++ design-pen/src/test/java/com/sanket/designpen/DesignPenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanket.designpen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
