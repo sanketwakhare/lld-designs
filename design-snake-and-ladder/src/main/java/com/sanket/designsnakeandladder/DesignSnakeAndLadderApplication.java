8a195256ef211ccd98e3eaddb876545432a54f8b
sanketwakhare <sanketwakhare@gmail.com>
2022-07-24 17:52:31 +0530

    snake and ladder setup

---
+++ design-snake-and-ladder/src/main/java/com/sanket/designsnakeandladder/DesignSnakeAndLadderApplication.java
@@ -0,0 +1,13 @@
+package com.sanket.designsnakeandladder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignSnakeAndLadderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignSnakeAndLadderApplication.class, args);
+    }
+
+}
