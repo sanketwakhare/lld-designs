861d00cc06db83a33a20efd687239e55ee42e883
sanketwakhare <sanketwakhare@gmail.com>
2022-07-31 15:20:09 +0530

    design parking lot setup

---
+++ design-parking-lot/src/test/java/com/sanket/designparkinglot/DesignParkingLotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanket.designparkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignParkingLotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
