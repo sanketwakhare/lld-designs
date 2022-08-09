4406bce6ba3e5fb77c2aa103f8c7aec36f860aaa
sanketwakhare <sanketwakhare@gmail.com>
2022-08-09 18:29:56 +0530

    setup code for splitwise

---
+++ design-splitwise/src/main/java/com/sanket/designsplitwise/DesignSplitwiseApplication.java
@@ -0,0 +1,13 @@
+package com.sanket.designsplitwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignSplitwiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignSplitwiseApplication.class, args);
+    }
+
+}
