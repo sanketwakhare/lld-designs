ade9ea5cdc091d5bb0f15673aa2ed9b63748a968
sanketwakhare <sanketwakhare@gmail.com>
2022-08-12 01:00:03 +0530

    update models

--- design-splitwise/src/main/java/com/sanket/designsplitwise/models/Currency.java
+++ design-splitwise/src/main/java/com/sanket/designsplitwise/models/Currency.java
@@ -3,11 +3,14 @@ package com.sanket.designsplitwise.models;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 @Getter
 @Setter
-public class Currency extends BaseModel{
+public class Currency extends BaseModel {
+
+    @Column(unique = true)
     private String name;
 }
