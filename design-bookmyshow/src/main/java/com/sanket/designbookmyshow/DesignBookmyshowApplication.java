b93fc83906d3f4c7571c28176eb7a4f6460b4997
sanketwakhare <sanketwakhare@gmail.com>
2022-08-20 19:27:42 +0530

    book my show project setup

---
+++ design-bookmyshow/src/main/java/com/sanket/designbookmyshow/DesignBookmyshowApplication.java
@@ -0,0 +1,13 @@
+package com.sanket.designbookmyshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignBookmyshowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignBookmyshowApplication.class, args);
+    }
+
+}
---
+++ design-bookmyshow/src/test/java/com/sanket/designbookmyshow/DesignBookmyshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanket.designbookmyshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignBookmyshowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
